ebb21c3c04ffa44aef0b17937a4a642867ce0a79
github username <github email address>
2021-06-13 23:47:13 -0400

    commit tree c32c1dc315fc3f5ad882c83f9286ef8e854e497b

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,18 @@ class Post < ActiveRecord::Base
     validates :content, length: { minimum: 10 }
     validates :summary, length: { maximum: 100 }
     validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
-    validates_with PostValidator
+    validate :is_clickbait?
+
+    CLICKBAIT_PATTERNS = [
+      /Won't Believe/i,
+      /Secret/i,
+      /Top [0-9]*/i,
+      /Guess/i
+    ]
+  
+    def is_clickbait?
+      if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+        errors.add(:title, "must be clickbait")
+      end
+    end
 end
